2aa8304b525051445026eb49bfc415ee76849557
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2024-01-07 22:57:56 +0200

    feat: server + mongoose

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const mongoURI: string = process.env.MONGODB_URI || "";
+
+if (!mongoURI) {
+  console.error("MongoDB URI is not provided in the environment variable.");
+  process.exit(1); 
+}
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    } as Parameters<typeof mongoose.connect>[1]);
+
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", (error as Error).message);
+  }
+};
